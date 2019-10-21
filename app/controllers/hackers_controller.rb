9bbe1db98c49d54cfd07e608ef66349ca2793815
memen <contactsaliens@gmail.com>
2019-10-20 21:40:08 -0500

    view

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -6,7 +6,7 @@ class HackersController < ApplicationController
         @result = @rank_result.calc_rank.sort_by {|obj| obj[:score]}.reverse.first(100)
 
         element = "<div> <table>\n"
-        element += "<thead><th><td>User</td><td>Info</td><td>Followers</td><td>Commits</td><td>Stars</td><td>Repos</td><td>Issues/PR</td></th><thead>\n"
+        element += "<thead><th><td>User</td><td>collaborations</td><td>stars</td><td>repos</td><td>fork</td></th><thead>\n"
         element += "<tbody>"
         @result.each_with_index do |user, i|
             element += "<tr><td>#{i + 1}</td>"
